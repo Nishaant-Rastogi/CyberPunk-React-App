cd904e7caac679bf75b636a23c1a4f2610a6cb4e
Nishaant-Rastogi <nishaant220091@iiitd.ac.in>
2021-12-17 01:01:08 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import { useState, useEffect } from 'react';
-import CollectionCard from './Components/CollectionCard';
 import Header from './Components/Header';
 import axios from 'axios'
 import PunkList from './Components/PunkList';
+import Main from './Components/Main';
 function App() {
   const [punkListData, setPunkListData] = useState([])
+  const [selectedPunk, setSelectedPunk] = useState(0);
   useEffect(() => {
     const getMyNFT = async () => {
       const openSeaData = await axios.get('https://testnets-api.opensea.io/assets?asset_contract_address=0x1Dbbe7663F856f8Ff5E57f3E538ba9D3209497D5&order_direction=asc')
@@ -17,7 +18,16 @@ function App() {
   return (
     <div className='app'>
       <Header />
-      <PunkList punkListData={punkListData} />
+      {
+        punkListData.length > 0 &&
+        (
+          <>
+            <Main selectedPunk={selectedPunk} punkListData={punkListData} />
+            <PunkList setSelectedPunk={setSelectedPunk} punkListData={punkListData} />
+          </>
+        )
+      }
+
     </div>
 
   );
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import './Main.css'
+import instagramLogo from '../assets/owner/instagram.png'
+import twitterLogo from '../assets/owner/twitter.png'
+import moreIcon from '../assets/owner/more.png'
 
-const Main = () => {
+const Main = ({ selectedPunk, punkListData }) => {
+    const [activePunk, setActivePunk] = useState(punkListData[0])
+    useEffect(() => {
+        setActivePunk(punkListData[selectedPunk])
+    }, [punkListData, selectedPunk])
     return (
-        <div>
-
+        <div className='mainContent'>
+            <div className="punkContainer">
+                <img src={activePunk?.image_original_url} alt="" className="selectedPunk" />
+            </div>
+            <div className="punkHighlight">
+                <div className="punkDetails" style={{ color: '#fff' }}>
+                    <div className="title">{activePunk?.name} <span className="itemNumber"> #{activePunk?.token_id}</span></div>
+                </div>
+                <div className="owner">
+                    <div className="ownerImageContainer">
+                        <img src={activePunk.owner.profile_img_url} alt="" />
+                    </div>
+                    <div className="ownerDetails">
+                        <div className="ownerNameAndHandle">
+                            <div className="address">{activePunk.owner.address}</div>
+                            <div className="ownerHandle">{activePunk.owner.handle}</div>
+                        </div>
+                        <div className="links">
+                            <div className="ownerLink">
+                                <img src={instagramLogo} alt="" />
+                            </div>
+                            <div className="ownerLink">
+                                <img src={twitterLogo} alt="" />
+                            </div>
+                            <div className="ownerLink">
+                                <img src={moreIcon} alt="" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
