87c756e3f9d8d4aa8762d5d42f3d55a67cfeabc0
Nishaant-Rastogi <nishaant220091@iiitd.ac.in>
2021-12-16 21:23:00 +0530

    first draft

---
+++ src/Components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Header.css'
+import punkLogo from '../assets/header/cryptopunk-logo.png'
+import searchIcon from '../assets/header/search.png'
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className="logoContainer">
+                <img src={punkLogo} className='punkLogo' alt="" />
+            </div>
+            <div className="searchBar">
+                <div className="searchIconContainer">
+                    <img src={searchIcon} alt="" />
+                </div>
+                <input type="text" className='searchInput' placeholder='Collections, item or user' />
+            </div>
+            <div className="headerItems">
+                <p>Drops</p>
+                <p>MarketPlace</p>
+                <p>Create</p>
+            </div>
+        </div>
+    )
+}
+
+export default Header
